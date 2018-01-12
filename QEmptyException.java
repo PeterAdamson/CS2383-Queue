eb569622ff88497bb082ddbf8961c7d0bc44cce4
PeterAdamson <adamsonpeterj@gmail.com>
2018-01-12 12:48:20 -0400

    Create QEmptyException.java

---
+++ QEmptyException.java
@@ -0,0 +1,13 @@
+//Author Peter Adamson
+
+public class QEmptyException extends Exception
+{
+	public QEmptyException()
+	{
+	}
+	
+	public QEmptyException(String message)
+	{
+		super(message);
+	}
+}
