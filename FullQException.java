00a877b181d11bf65132a10ab755c72229e09852
PeterAdamson <adamsonpeterj@gmail.com>
2018-01-12 12:48:00 -0400

    Create FullQException.java

---
+++ FullQException.java
@@ -0,0 +1,13 @@
+//Author Peter Adamson
+
+public class FullQException extends Exception
+{
+	public FullQException()
+	{
+	}
+	
+	public FullQException(String message)
+	{
+		super(message);
+	}
+}
